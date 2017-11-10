9849d9a4e7e3cbf4aef374ac12e1c1a445e47854
Cynthia <Cynthia@Cynthia-PC>
2017-11-10 14:29:11 -0300

    codigo generado

--- src/Tercetos/TercetoSuma.java
+++ src/Tercetos/TercetoSuma.java
@@ -8,18 +8,15 @@ public class TercetoSuma extends Terceto{
 	
 		private String aux1;
 		private String aux2;
-
 		private String s1;
 		private String s2;
 		private String s3;
-		
 		private ParserVal primero;
 		private ParserVal segundo;
 		private int pos;
 
 	public TercetoSuma(ParserVal primero, ParserVal segundo, int pos) {
-		super("+", primero, segundo, pos);
-				
+		super("+", primero, segundo, pos);		
 		this.primero = primero;
 		this.segundo = segundo;
 		this.pos = pos;
@@ -29,33 +26,33 @@ public class TercetoSuma extends Terceto{
 	{
 		if(primero.obj != null) {
 			aux1 = String.valueOf(((Terceto)primero.obj).getPos()); 
-			s1="MOV R1 #aux"+aux1;
+			s1="MOV R1,#aux"+aux1;
 		}
 		else {
 			aux1 = primero.sval;
 			if(Sintactico.esVariable(primero)) {
-				s1="MOV R1 _"+aux1;
+				s1="MOV R1,_"+aux1;
 			}
 			else {
-				s1="MOV R1 "+aux1;	
+				s1="MOV R1,"+aux1;	
 			}
 		}
 		
 		if(segundo.obj != null) {
 			aux2= String.valueOf(((Terceto)segundo.obj).getPos());
-			s2="ADD R1 #aux"+aux2;
+			s2="ADD R1,#aux"+aux2;
 		}
 		else {
 			aux2 = segundo.sval;	
 			if(Sintactico.esVariable(segundo)) {
-				s2="ADD R1 _"+aux2;
+				s2="ADD R1,_"+aux2;
 			}
 			else {
-				s2="ADD R1 "+aux2;	
+				s2="ADD R1,"+aux2;	
 			}
 			
 		}
-		s3="MOV #aux"+ pos + " R1";
+		s3="MOV #aux"+ pos + ",R1";
 	
 		Lexico.putSimboloAsm("#aux"+pos);
 		
