760b8f38318700fd4bd7d7170756f7cf8b30a4c7
Cynthia <Cynthia@Cynthia-PC>
2017-11-18 11:50:43 -0300

    barbie acomodo la suma

--- src/Tercetos/TercetoSuma.java
+++ src/Tercetos/TercetoSuma.java
@@ -22,46 +22,59 @@ public class TercetoSuma extends Terceto{
 		
 	public String getCodigo()
 	{
-		if(primero.obj != null) {
+		s1="Barbie";
+		if(primero.obj != null) { //es un terceto anterior............. acomodar
 			aux1 = String.valueOf(((Terceto)primero.obj).getPos()); 
-			s1="MOV R1,#aux"+aux1;
+			s1="MOV R1,#aux" + aux1 + "\n"; 
 		}
 		else {
 			aux1 = primero.sval;
-			if(Sintactico.esVariable(primero)) {
-				s1="MOV R1,_"+aux1;
+			if(Sintactico.esVariable(primero)) { //es una variable
+				String tipo=this.generador.getSintactico().getLexico().getTokenFromTS(aux1+"@Variable").getTipoDato();
+				if (tipo.equals("DOUBLE")) { //es una variable de tipo DOUBLE
+					s1="FLD " + aux1 + "@Variable";
+				}
+				else { //es una variable de tipo LONG
+					s1="MOV R1,_" + aux1 + "\n";
+				}
 			}
 			else {
-				if (aux1.toString().contains(",")) {
-					//System.out.println("es un DOUBLE");
-					s1="FLD "+aux1;
+				if (aux1.toString().contains(",")) { //es una constante de tipo DOUBLE
+					s1="FLD " + aux1;
 				}
-				else {
-					s1="MOV R1,"+aux1;
+				else { //es una constante de tipo LONG
+					s1="MOV R1," + aux1 + "\n"; 
 				}
 			}
 		}
 		
-		if(segundo.obj != null) {
+		if(segundo.obj != null) { //acomodar
 			aux2= String.valueOf(((Terceto)segundo.obj).getPos());
-			s2="ADD R1,#aux"+aux2;
-			s3="MOV #aux"+ this.getPos()+ ",R1";
+			s2="ADD R1,#aux" + aux2 + "\n";
+			s3="MOV #aux"+ this.getPos()+ ",R1" + "\n";
 		}
 		else {
 			aux2 = segundo.sval;	
 			if(Sintactico.esVariable(segundo)) {
-				s2="ADD R1,_"+aux2;
-				s3="MOV #aux"+ this.getPos()+ ",R1" + "\n" + "JO _overflow\n";
 				String tipo=this.generador.getSintactico().getLexico().getTokenFromTS(aux2+"@Variable").getTipoDato();
+				if (tipo=="DOUBLE") {
+					s2="FLD " + aux1 + "@Variable";
+					s3="FADD" + "\n" + "FSTP #aux" + this.getPos() + "\n";
+				}
+				else
+				{
+					s2="ADD R1,_" + aux2 + "\n";
+					s3="MOV #aux"+ this.getPos()+ ",R1" + "\n" + "JO _overflow\n";
+				}
 			}
 			else {
 				if (aux2.toString().contains(",")) {
 					//System.out.println("es un DOUBLE");
 					s2= "FLD " + aux2;
-					s3="FADD" + "\n" + "FSTP #aux" + this.getPos(); //detalle: aux tiene q ser declarada como flotante. 
+					s3="FADD" + "\n" + "FSTP #aux" + this.getPos() + "\n"; //detalle: aux tiene q ser declarada como flotante. 
 				}
 				else {
-					s2="ADD R1,"+aux2;
+					s2="ADD R1," + aux2;
 					s3="MOV #aux"+ this.getPos()+ ",R1" + "\n" + "JO _overflow\n";
 				}
 			}
