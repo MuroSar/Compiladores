d39f2abe15a4807ea06d3874c96789c6452d1e72
MauroEsc <MauroEsc@DESKTOP-FDH3AAV>
2017-11-30 16:26:28 -0300

    dasd

--- src/Tercetos/TercetoMultiplicacion.java
+++ src/Tercetos/TercetoMultiplicacion.java
@@ -31,13 +31,13 @@ public class TercetoMultiplicacion extends Terceto{
 			aux1 = String.valueOf(((Terceto)primero.obj).getPos()); 
 			String tipo = ((Terceto)primero.obj).getTipoDato();
 			if (tipo.equals("DOUBLE")) {
-				String limpiaPila = "FFREE ST(0)\n" + "FFREE ST(1)\n" + "FWAIT\n";
+				//String limpiaPila = "FFREE ST(0)\n" + "FFREE ST(1)\n" + "FWAIT\n";
 				if (((Terceto)primero.obj).getOperador().equals("FN")) {
 					String nombre_func = ((Terceto)primero.obj).getPrimero() + "@Funcion";
-					s1= limpiaPila + "FLD " + nombre_func ;//+ "\n" + "FSTP " + aux1 +"\n";
+					s1= "FLD " + nombre_func ;//+ "\n" + "FSTP " + aux1 +"\n";
 				}
 				else {//no es funcion
-					s1= limpiaPila + "FLD var@@aux" + aux1;
+					s1= "FLD var@@aux" + aux1;
 				}
 				//tokenAux.setTipoDato("DOUBLE");
 			}
@@ -57,8 +57,8 @@ public class TercetoMultiplicacion extends Terceto{
 			if(Sintactico.esVariable(primero)) { //es una variable
 				String tipo=this.generador.getSintactico().getLexico().getTokenFromTS(aux1+"@Variable").getTipoDato();
 				if (tipo.equals("DOUBLE")) { //es una variable de tipo DOUBLE
-					String limpiaPila = "FFREE ST(0)\n" + "FFREE ST(1)\n" + "FWAIT\n";
-					s1= limpiaPila + "FLD " + aux1 + "@Variable";
+					//String limpiaPila = "FFREE ST(0)\n" + "FFREE ST(1)\n" + "FWAIT\n";
+					s1= "FLD " + aux1 + "@Variable";
 				}
 				else { //es una variable de tipo LONG
 					s1="MOV EAX,"+aux1+ "@Variable"; 
@@ -70,8 +70,8 @@ public class TercetoMultiplicacion extends Terceto{
 					if(!this.generador.delcaracionesConstContains(aux)) {
 						this.generador.setDeclaracionesConst(aux);	
 					}		
-					String limpiaPila = "FFREE ST(0)\n" + "FFREE ST(1)\n" + "FWAIT\n";
-					s1= limpiaPila + "FLD const@@"+aux1.replace(',', '_');
+					//String limpiaPila = "FFREE ST(0)\n" + "FFREE ST(1)\n" + "FWAIT\n";
+					s1= "FLD const@@"+aux1.replace(',', '_');
 				}
 				else { //es una constante de tipo LONG
 					s1="MOV EAX,"+aux1;
